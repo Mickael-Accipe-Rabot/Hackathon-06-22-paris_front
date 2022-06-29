0336dbf5133174770d862ab08c97c7af337c8c56
ACCIPE-RABOT MICKAEL <mickael.accipe.rabot@gmail.com>
2022-06-29 19:16:14 +0200

    projectslist hardcode

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './projectCard.css';
 const ProjectCard = ({ project, color }) => {
+    
   return (
     <div className="">
-      <Link to={`/project-detail${project.id}`}>
+      {/* <Link to={`/project-detail${project.id}`}> */}
         <div
-          className="projectCard-color"
-          style={{ backgroundColor: color }}
+          className="projectCard-container"
+        //   style={{ backgroundColor: color }}
         >
-            <h3>{project.name}</h3>
+            {/* <h3>{project.name}</h3> */}
+            {/* <h3>{project.enterprise}</h3> */}
+            <h3>name</h3>
+            <h3>enterprise</h3>
+            <p>Techno : React/Node</p>
+            <p>description</p>
         </div>
-      </Link>
+      {/* </Link> */}
     </div>
   );
 };
