46f818ea980d6aebe7b36c5ac96efa36b6fbcdd6
ACCIPE-RABOT MICKAEL <mickael.accipe.rabot@gmail.com>
2022-06-30 13:01:04 +0200

    project card & list css

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './projectCard.css';
 const ProjectCard = ({ project, color }) => {
-    
+
+const [isActiv, setIsActiv]=useState(true);
+
   return (
-    <div className="">
+    <div className="projectCard-container">
       {/* <Link to={`/project-detail${project.id}`}> */}
-        <div
-          className="projectCard-container"
+      {/* <div
+        className=""
         //   style={{ backgroundColor: color }}
-        >
-            {/* <h3>{project.name}</h3> */}
-            {/* <h3>{project.enterprise}</h3> */}
-            <h3>name</h3>
-            <h3>enterprise</h3>
-            <p>Techno : React/Node</p>
-            <p>description</p>
+      > */}
+        {/* <h3>{project.name}</h3> */}
+        {/* <h3>{project.enterprise}</h3> */}
+        <div className="project-title">
+       
+          <h3 className='project-name'>
+          <img
+              src="https://randomwordgenerator.com/img/picture-generator/57e1d1444252af14f1dc8460962e33791c3ad6e04e5074417c2f73d59f49c5_640.jpg"
+              alt="#"
+              className="project-img"
+            />
+            Project
+          </h3>
+          <p className={isActiv ? 'status-tag':"status-tag-over" }>status</p>
         </div>
+
+        <div className="client-infos">
+          <p className='client-name'>Client name</p>
+          <p className='client-date'>Start date</p>
+          <p className='client-start'>Timing</p>
+        </div>
+
+        <div className="stacks-container">
+          <p className="stack-tag">Stacks</p>
+          <p className="stack-tag">Stacks</p>
+          <p className="stack-tag">Stacks</p>
+        </div>
+      {/* </div> */}
       {/* </Link> */}
     </div>
   );
--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -18,7 +18,16 @@ const ProjectsList = () => {
 
   return (
     <div className='projectsList-container'>
-      ProjectsList
+      <ul className='Projects-subMenu'>
+        <li className='subMenu-title'>Projects</li>
+        <div className="subMenu-infos">
+        <li>Clients</li>
+        <li>Sart date</li>
+        <li>Timing</li>
+        </div>
+        <li className='subMenu-stacks'>Stacks</li>
+        <li className='subMenu-status'>Status</li>
+      </ul>
       {/* {projects &&
         projects.map((project, index) => (
           <div key={index}>
