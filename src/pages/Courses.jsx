04d11620792ffcc872c96cd2bf2b6253510a5e28
ACCIPE-RABOT MICKAEL <mickael.accipe.rabot@gmail.com>
2022-07-01 11:28:27 +0200

    lastone

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -3,13 +3,14 @@ import axios from 'axios';
 import CoursesList from '../components/CoursesList';
 import CreateCourse from '../components/buttons/CreateCourse';
 import Navbar from '../components/nav/Navbar';
+import './courses.css'
 
 
 const Courses = () => {
   return (
     <div className="page courses-container">
       <Navbar/>
-      <h1>Courses</h1>
+      <h1 className='course-title-page'>Courses</h1>
       <CreateCourse />
       <CoursesList />
     </div>
